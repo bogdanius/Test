bbcd5bee5f911f00fdad800ee28b8dbecf54f9a4
Bogdan <bagi.knes@gmail.com>
2015-07-22 00:25:49 +0300

    added facebook auth and posting

--- app/src/main/java/bogdan/test/app/MainActivity.java
+++ app/src/main/java/bogdan/test/app/MainActivity.java
@@ -2,58 +2,81 @@ package bogdan.test.app;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.*;
 import android.widget.Button;
 import android.widget.EditText;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
+import android.widget.Toast;
+import com.facebook.*;
+import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.facebook.share.Sharer;
+import com.facebook.share.model.ShareContent;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareDialog;
 
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
     private Button buttonSetValue;
     private Button parcelSetValue;
-    private LoginButton loginButton;
+    private Button buttonSetMessage;
+    private EditText editTextSetMessage;
     private EditText editTextInputValue;
     private CallbackManager callbackManager;
+
+    private ShareDialog shareDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
-
         buttonSetValue = (Button) findViewById(R.id.bt_set_value);
         parcelSetValue = (Button) findViewById(R.id.bt_parcel_set_value);
+        buttonSetMessage = (Button) findViewById(R.id.button_facebook);
         editTextInputValue = (EditText) findViewById(R.id.et_input_value);
+        editTextSetMessage = (EditText) findViewById(R.id.edit_text_facebook);
 
         buttonSetValue.setOnClickListener(this);
         parcelSetValue.setOnClickListener(this);
-
-        FacebookSdk.sdkInitialize(getApplicationContext());
+        buttonSetMessage.setOnClickListener(this);
         callbackManager = CallbackManager.Factory.create();
+
         LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("user_friends");
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-
+                Toast.makeText(getApplicationContext(), "onSuccess", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onCancel() {
-
+                Toast.makeText(getApplicationContext(), "onCancel", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onError(FacebookException e) {
-
+                Toast.makeText(getApplicationContext(), "onError", Toast.LENGTH_SHORT).show();
             }
         });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        AppEventsLogger.deactivateApp(this);
     }
 
     @Override
@@ -85,29 +108,42 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 intentParceble.putExtra("intentParcelable", cardParcelable);
                 startActivity(intentParceble);
                 break;
+
+            case R.id.button_facebook:
+                postToFacebook();
+                break;
         }
     }
 
+    private void postToFacebook() {
+        shareDialog = new ShareDialog(this);
+        shareDialog.registerCallback(callbackManager, new FacebookCallback<Sharer.Result>() {
+            @Override
+            public void onSuccess(Sharer.Result result) {
+                Toast.makeText(getApplicationContext(), "onSuccessPost", Toast.LENGTH_SHORT).show();
+            }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+            @Override
+            public void onCancel() {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            @Override
+            public void onError(FacebookException e) {
+                Toast.makeText(getApplicationContext(), "onError", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Log.d("Facebook", "if");
+        if (ShareDialog.canShow(ShareLinkContent.class)) {
+            ShareContent shareContent = new ShareLinkContent.Builder()
+                    .setContentTitle("Hello Facebook")
+                    .setContentDescription(
+                            "The 'Hello Facebook' sample  showcases simple Facebook integration")
+                    .setContentUrl(Uri.parse("http://developers.facebook.com/android"))
+                    .build();
+            Log.d("Facebook", "if - true");
+            shareDialog.show(shareContent);
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
 }
